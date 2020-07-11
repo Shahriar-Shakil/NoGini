1228b446ac0956747c8b0824b8927afa033e82ff
shahriar-shakil <toshahriarshakil@gmail.com>
2020-07-11 16:45:13 +0600

    Updates

---
+++ precache-manifest.8c6302571c2c67f6c73704f177fd2263.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1127d04f147bc376eed7ebbb80a3d94b",
+    "url": "/NoGini/index.html"
+  },
+  {
+    "revision": "aa8cd5facf100ef8776f",
+    "url": "/NoGini/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "85d219586dfc6a209cb2",
+    "url": "/NoGini/static/js/2.bcd77ea1.chunk.js"
+  },
+  {
+    "revision": "aa8cd5facf100ef8776f",
+    "url": "/NoGini/static/js/main.3303dbaf.chunk.js"
+  },
+  {
+    "revision": "e3a18d45f6947a942e21",
+    "url": "/NoGini/static/js/runtime-main.4a816e3e.js"
+  }
+]);
